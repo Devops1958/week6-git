8c73f3fb54607c330dac8f9cbdc2d2c19b2de6a1
unknown <nyoyo@gmail.com>
2024-01-30 21:19:19 -0500

    m

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-resource "aws_lightsail_instance" "server1"{
+resource "aws_lightsail_instance" "server"{
     name = "dev-server"
     blueprint_id = "ubuntu_18_04"
-    bundle_id = "mediun_1_0"
+    bundle_id = "medium_1_0"
     availability_zone = "us-east-1a"
-    user_data = <<- EOF
+    user_data = <<-E0F
     
     
 }
-    
