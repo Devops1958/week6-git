0d54a26216273f0dfc3a836afac56802a643a112
unknown <nyoyo@gmail.com>
2024-01-30 19:40:19 -0500

    lightsail

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_lightsail_instance" "server1"{
+    name = "dev-server"
+    blueprint_id = "ubuntu_18_04"
+    bundle_id = "mediun_1_0"
+    availability_zone = "us-east-1a"
+    user_data = <<- EOF
+    
+    
+}
+    
